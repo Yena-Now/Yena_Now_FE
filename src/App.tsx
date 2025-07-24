17457ea53f2f8ba78ee2c516793073665096a9fe
ryongseong <ryongseong.dev@gmail.com>
2025-07-24 15:21:11 +0900

    initial: FE 프로젝트 초기 세팅
    - 2025-07-24 진행
    
    S13P11E203-19 #done 작업 완료

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import GlobalStyle from '@styles/GlobalStyle'
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div>
+        <main></main>
+      </div>
+    </>
+  )
+}
+
+export default App
