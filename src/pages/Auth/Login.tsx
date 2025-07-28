611937b7f658b2e675fd66d3880502f19af64c18
yena <yeneua.dev@gmail.com>
2025-07-28 17:38:21 +0900

    feat: 로그인 페이지 퍼블리싱

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Logo from '@components/Common/Logo'
 import { Link } from 'react-router-dom'
+import * as S from '@styles/pages/Auth/LoginStyle'
+import * as T from '@styles/pages/Auth/AuthGlobalStyle'
 
 const Login: React.FC = () => {
   return (
     <>
-      <h1>현재 회원가입 구현 중이기에 회원가입에 대해서만 생각</h1>
-      <Link to="/signup">
-        <button>회원가입</button>
-      </Link>
+      <T.Layout>
+        <T.SignupContainer>
+          <Logo />
+          <T.Input type="email" name="email" placeholder="이메일" />
+          <T.Input type="email" name="email" placeholder="비밀번호" />
+          <T.Button type="button">로그인</T.Button>
+          <Link to="/signup">
+            <T.Button type="button">회원가입</T.Button>
+          </Link>
+          <S.OptionSection>
+            <S.AutoLoginBox>
+              <S.CheckBox type="checkbox" id="autoLogin" />
+              <S.Text htmlFor="autoLogin">자동 로그인</S.Text>
+            </S.AutoLoginBox>
+            <Link to="#">
+              <S.PasswordButton>비밀번호 재설정</S.PasswordButton>
+            </Link>
+          </S.OptionSection>
+          <S.Divider>또는</S.Divider>
+          <T.Button type="button">구글로그인넣기</T.Button>
+          <T.Button type="button">카카오로그인넣기</T.Button>
+        </T.SignupContainer>
+      </T.Layout>
     </>
   )
 }
