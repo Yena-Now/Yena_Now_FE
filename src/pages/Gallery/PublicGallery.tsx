204739552b38d154a494cf6b722138b584912d8c
DESKTOP-DLOPF19\SSAFY <tjdusgml1219@naver.com>
2025-07-30 21:46:28 +0900

    feat: GalleryList 컴포넌트로 분리 및 스타일 분리

--- src/pages/Gallery/PublicGallery.tsx
+++ src/pages/Gallery/PublicGallery.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import Header from '@components/Header/Header'
-import GalleryCard from '@components/Common/GalleryCard'
-import type { NCut, NCutList } from '@/types/NCutList'
+import GalleryList from '@components/Gallery/GalleryList'
+import type { NCut } from '@/types/NCutList'
 import { dummyNCutList } from './DummyNcutList'
 import * as S from '@styles/pages/Gallery/PublicGalleryStyles'
 
@@ -25,15 +25,7 @@ const PublicGallery: React.FC<NCut> = () => {
         <S.FriendText onClick={goToFollowingGallery}>친구 갤러리</S.FriendText>
       </S.TitleWrapper>
       <S.Divider />
-      <S.GalleryList>
-        {dummyNCutList.ncuts.map((item) => (
-          <GalleryCard
-            key={item.ncut_uuid}
-            {...item}
-            onClick={() => handleClick(item)}
-          />
-        ))}
-      </S.GalleryList>
+      <GalleryList data={dummyNCutList.ncuts} onItemClick={handleClick} />
     </>
   )
 }
