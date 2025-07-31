055599b23bfbe7822e762fb82664f35a6c38eea6
DESKTOP-DLOPF19\SSAFY <tjdusgml1219@naver.com>
2025-07-31 22:24:06 +0900

    refactor: 갤러리 컴포넌트 구조 정리 및 불필요 파일 제거

--- src/styles/components/Common/GalleryTabMenu.ts
+++ src/styles/components/Common/GalleryTabMenu.ts
@@ -1,13 +1,25 @@
 import styled from 'styled-components'
 
-export const PublicText = styled.span`
+export const GalleryTabMenu = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  margin-bottom: 8px;
+`
+
+export const GalleryTab = styled.span`
   display: flex;
   padding: 50px 0 20px 90px;
   font-size: 16px;
   color: gray;
+  border: none;
   cursor: pointer;
   &:hover {
     text-decoration: underline;
+  }
+  &.active {
+    color: black;
+    font-weight: 450;
   }
 `
 
@@ -16,16 +28,4 @@ export const Divider = styled.hr`
   border-bottom: 2px solid #ddd;
   margin-bottom: 30px;
   width: 90%;
-`
-export const TitleWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 12px;
-  margin-bottom: 8px;
-`
-
-export const Text = styled.span`
-  display: flex;
-  padding: 50px 0 20px 60px;
-  font-size: 16px;
 `
