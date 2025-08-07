a11246686aaee55d8194d55e6bc1329bb69cfc22
yena <yeneua.dev@gmail.com>
2025-08-07 23:52:28 +0900

    fix: 회원 정보 수정 후 바뀐 데이터가 바로 표시되지 않는 문제 수정
    - 2025.08.07 진행
    - MyProfileInfo 컴포넌트에서 api 호출하는 fetchMyInfo 함수를 useCallback으로 작성
    
    S13P11E203-194 #done

--- src/pages/MyProfile/MyProfileInfo.tsx
+++ src/pages/MyProfile/MyProfileInfo.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { userAPI } from '@api/user'
 import type { UserMeResponse } from '@/types/User'
@@ -12,15 +12,16 @@ const MyProfileInfo: React.FC = () => {
   const isEdit = searchParams.has('edit')
   const [myInfo, setMyInfo] = useState<UserMeResponse | null>(null)
 
-  useEffect(() => {
-    const fetchMyInfo = async () => {
-      try {
-        const myData = await userAPI.getUserMeInfo()
-        setMyInfo(myData)
-      } catch {
-        error('내 정보를 불러오는 데 실패했습니다.')
-      }
+  const fetchMyInfo = useCallback(async () => {
+    try {
+      const myData = await userAPI.getUserMeInfo()
+      setMyInfo(myData)
+    } catch {
+      error('내 정보를 불러오는 데 실패했습니다.')
     }
+  }, [])
+
+  useEffect(() => {
     fetchMyInfo()
   }, [])
 
@@ -31,7 +32,7 @@ const MyProfileInfo: React.FC = () => {
           {isEdit ? (
             <ProfileEdit myInfo={myInfo} />
           ) : (
-            <ProfileView myInfo={myInfo} />
+            <ProfileView myInfo={myInfo} fetchMyInfo={fetchMyInfo} />
           )}
         </>
       )}
